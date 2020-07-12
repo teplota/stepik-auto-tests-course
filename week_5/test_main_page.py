2b619a889a2eeb2a0739a6fb5cb2dd16881f5703
teplota <1729@inbox.ru>
2020-07-12 22:26:11 +0300

    Corrected misprint

--- week_5/test_main_page.py
+++ week_5/test_main_page.py
@@ -3,14 +3,14 @@ from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # Инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # Открываем страницу
-    page.go_to_login_page()          # Выполняем метод страницы - переходим на страницу логина
+    page = MainPage(browser, link)                    # Инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                                       # Открываем страницу
+    page.go_to_login_page()                           # Выполняем метод страницы - переходим на страницу логина
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # Инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # Открываем страницу
+    page = MainPage(browser, link)                    # Инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                                       # Открываем страницу
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()      # Выполняем проверку
+    login_page.should_be_login_page()                 # Выполняем проверку
