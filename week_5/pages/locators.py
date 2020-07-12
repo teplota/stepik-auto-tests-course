1a645a70bbcd7dffc12a81da36b8572f06f4880c
teplota <1729@inbox.ru>
2020-07-12 22:28:25 +0300

    Added locators for registration page

--- week_5/pages/locators.py
+++ week_5/pages/locators.py
@@ -15,3 +15,10 @@ class ProductPageLocators():
     MESSAGE_TITLE = (By.CSS_SELECTOR, "#messages .alert-success")
     MESSAGE_PRICE = (By.CSS_SELECTOR, "#messages .alert-info")
 
+class RegistrationLocators():
+    EMAIL = (By.ID, 'id_registration-email')
+    PASSWORD1 = (By.ID, 'id_registration-password1')
+    PASSWORD2 = (By.ID, 'id_registration-password2')
+    REGISTRATION_BUTTON = (By.NAME, 'registration_submit')
+    REGISTRATION_MESSAGE = (By.CSS_SELECTOR, '#messages .alert-success')
+
