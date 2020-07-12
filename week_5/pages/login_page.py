db0fe6df1e69b9f65cd9313943eefcd89d8f110c
teplota <1729@inbox.ru>
2020-07-12 11:58:54 +0300

    Corrected locators, url check

--- week_5/pages/login_page.py
+++ week_5/pages/login_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
+from .locators import LoginPageLocators
 from selenium.webdriver.common.by import By
 
 class LoginPage(BasePage):
@@ -10,13 +11,13 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        self.current_url()
-        assert "/login" in current_url.text, "Wrong login url"
+        check_current_url = self.check_current_url()
+        assert "/login" in check_current_url, "Wrong login url"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(*MainPageLocators.LOGIN_FORM), "Login form is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(*MainPageLocators.REGISTER_FORM), "Register form is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
