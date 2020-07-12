3008f7ccafeb8d1bcd73fd4ba251912c7527f612
teplota <1729@inbox.ru>
2020-07-12 22:24:56 +0300

    added Chrome

--- week_5/conftest.py
+++ week_5/conftest.py
@@ -3,16 +3,21 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='en')
+    parser.addoption('--browser', action='store', default='Chrome')
+    parser.addoption('--language', action='store', default='en-gb')
 
 @pytest.fixture(scope="function")
 def browser(request):
-    print("\nstart browser for test..")
+    user_browser = request.config.getoption('browser')
     user_language = request.config.getoption('language')
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
-    browser.implicitly_wait(10)
+    if user_browser == "Chrome":
+        print("\nstart browser for test..")
+        browser = webdriver.Chrome(options=options)
+        browser.implicitly_wait(10)
+    else:
+        print('Browser is not supported')
     yield browser
     print("\nquit browser..")
     browser.quit()
