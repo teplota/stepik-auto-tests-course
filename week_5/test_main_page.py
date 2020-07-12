cadf1e4f30ea25ab35f712fa275c3b45474c192e
teplota <1729@inbox.ru>
2020-07-12 11:54:46 +0300

    Added transaction between pages

--- week_5/test_main_page.py
+++ week_5/test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -10,4 +11,6 @@ def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # Инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # Открываем страницу
-    page.should_be_login_link()      # Выполняем метод страницы - переходим на страницу логина
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()      # Выполняем проверку
