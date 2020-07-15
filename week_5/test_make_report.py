425e651ed6f240a3013b4176a488c5f35ae6d565
teplota <1729@inbox.ru>
2020-07-15 23:53:55 +0300

    Added 'registration_text' variable for different languages

--- week_5/test_make_report.py
+++ week_5/test_make_report.py
@@ -4,11 +4,11 @@ from .pages.login_page import LoginPage
 
 
 
-def test_guest_can_register_unique_email(browser):
+def test_guest_can_register_unique_email(browser, registration_text):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)                               # Инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                                                  # Открываем страницу
     page.go_to_login_page()                                      # Переходим на страницу логина
     registration_page = RegistrationPage(browser, browser.current_url)
     registration_message = registration_page.new_user_registration()
-    assert "Thanks for registering!" in registration_message.text, 'Wrong registration message'
+    assert registration_text in registration_message.text, 'Wrong registration message'
