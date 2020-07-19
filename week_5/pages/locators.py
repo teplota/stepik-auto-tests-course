68f992820e9d2adab00ad8707b7b1e26e4b5d77a
teplota <1729@inbox.ru>
2020-07-20 00:00:40 +0300

    Add locators for user login

--- week_5/pages/locators.py
+++ week_5/pages/locators.py
@@ -7,18 +7,20 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
-
-class ProductPageLocators():
-    TITLE = (By.CSS_SELECTOR, ".product_page h1")
-    PRICE = (By.CSS_SELECTOR, ".product_page .price_color")
-    ADD_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form button")
-    MESSAGE_TITLE = (By.CSS_SELECTOR, "#messages .alert-success")
-    MESSAGE_PRICE = (By.CSS_SELECTOR, "#messages .alert-info")
-
-class RegistrationLocators():
+    EMAIL_LOGIN = (By.ID, 'id_login-username')
+    PASSWORD_LOGIN = (By.ID, 'id_login-password')
+    LOGIN_BUTTON = (By.NAME, 'login_submit')
     EMAIL = (By.ID, 'id_registration-email')
     PASSWORD1 = (By.ID, 'id_registration-password1')
     PASSWORD2 = (By.ID, 'id_registration-password2')
     REGISTRATION_BUTTON = (By.NAME, 'registration_submit')
     REGISTRATION_MESSAGE = (By.CSS_SELECTOR, '#messages .alert-success')
+    ERROR_REGISTRATION_MESSAGE = (By.CSS_SELECTOR, '#register_form .alert-danger')
+    ERROR_REGISTRATION_MESSAGE_BELOW = (By.CSS_SELECTOR, '.has-error .error-block')
 
+class ProductPageLocators():
+    TITLE = (By.CSS_SELECTOR, ".product_page h1")
+    PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    ADD_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form button")
+    MESSAGE_TITLE = (By.CSS_SELECTOR, "#messages .alert-success strong")
+    MESSAGE_PRICE = (By.CSS_SELECTOR, "#messages .alert-info strong")
