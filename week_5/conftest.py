d78da40f992142c5b7bb7c191d2a197f798d2039
teplota <1729@inbox.ru>
2020-07-19 23:54:21 +0300

    Added variable for email and password, added fixture for testing registration message

--- week_5/conftest.py
+++ week_5/conftest.py
@@ -33,3 +33,61 @@ def registration_text(request):
         'ru': 'Спасибо за регистрацию!'
     }
     return registration_text[user_language]
+
+@pytest.fixture(scope="function")
+def error_registration_text(request):
+    user_language = request.config.getoption('language')
+    error_registration_text = {
+        'en-gb': 'Oops! We found some errors - please check the error messages below and try again',
+        'de': 'Ups! Es sind Fehler aufgetreten - bitte korrigieren Sie die untenstehenden Fehler und versuchen Sie es erneut',
+        'es': 'Oops! Encontramos algunos errores - Por favor, comprueba el mensaje de error siguiente e inténtalo de nuevo',
+        'fr': "Oups ! Nous avons trouvé des erreurs - veuillez vérifier les messages d'erreur ci-dessous et réessayer",
+        'ru': 'Опаньки! Мы нашли какие-то ошибки - пожалуйста, проверьте сообщения об ошибках ниже и попробуйте еще раз'
+    }
+    return error_registration_text[user_language]
+
+@pytest.fixture(scope="function")
+def error_registration_message_below_email_already_exist(request):
+    user_language = request.config.getoption('language')
+    error_registration_message_below_email_already_exist = {
+        'en-gb': "A user with that email address already exists",
+        'ru': 'Два поля с паролями не совпадают.'
+    }
+    return error_registration_message_below_email_already_exist[user_language]
+
+@pytest.fixture(scope="function")
+def error_registration_message_below_password_did_not_match(request):
+    user_language = request.config.getoption('language')
+    error_registration_message_below_password_did_not_match = {
+        'en-gb': "The two password fields didn't match.",
+        'ru': 'Два поля с паролями не совпадают.'
+    }
+    return error_registration_message_below_password_did_not_match[user_language]
+
+@pytest.fixture(scope="function")
+def email_already_exist():
+    email_already_exist = 'dohapa4146@lefaqr5.com' #Зарегистрированный ранее пользователь
+    return email_already_exist
+
+@pytest.fixture(scope="function")
+def password_already_exist():
+    password_already_exist = 'Selenium111'          #Пароль зарегистрированого ранее пользователя
+    return password_already_exist
+
+@pytest.fixture(scope="function")
+def email_invalid():
+    email_invalid = '111'
+    return email_invalid
+
+@pytest.fixture(scope="function")
+def password_valid():
+    password_valid = 'veryHardP147'
+    return password_valid
+
+@pytest.fixture(scope="function")
+def password_invalid():
+    password_invalid = '111'
+    return password_invalid
+
+
+
