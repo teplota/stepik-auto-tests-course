a4d26e7e12013bdd8a8ec069d7c46162911b0faf
teplota <1729@inbox.ru>
2020-07-22 13:50:30 +0300

    Added negative tests for registering new user

--- week_5/test_login_page.py
+++ week_5/test_login_page.py
@@ -1,16 +1,14 @@
 import pytest
 from .pages.base_page import BasePage
 from .pages.login_page import LoginPage
-from random import randint
 
 
-def test_guest_can_register_unique_email(browser, password_valid, registration_text):
+def test_guest_can_register_unique_email(browser, email, password_valid, registration_text):
     link = "http://selenium1py.pythonanywhere.com/"
     page = BasePage(browser, link)
     page.open()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
-    email = f'new_email-{randint(1, 10000)}@email.com'
     password1 = password2 = password_valid
     login_page.new_user_registration(email, password1, password2)
     assert login_page.check_should_be_logout_link(),"Logout link is not presented"
@@ -32,19 +30,57 @@ def test_guest_can_not_register_email_already_exist(browser, email_already_exist
     assert error_registration_message_below_email_already_exist == login_page.should_be_error_registration_message_below(), 'Wrong error registration message below'
 
 @pytest.mark.need_review_custom_scenarios
-def test_guest_can_not_register_password_repeat_did_not_match(browser, password_valid, error_registration_text, error_registration_message_below_password_did_not_match):
+def test_guest_can_not_register_email_invalid(browser, email_invalid, password_valid, error_registration_text):
     link = "http://selenium1py.pythonanywhere.com/"
     page = BasePage(browser, link)
     page.open()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
-    email = f'new_email-{randint(1, 10000)}@email.com'
+    email = email_invalid
+    password1 = password2 = password_valid
+    login_page.new_user_registration(email, password1, password2)
+    error_registration_message = login_page.should_not_be_registration_successful()
+    assert error_registration_text == error_registration_message, 'Wrong error registration message'
+
+@pytest.mark.need_review_custom_scenarios
+def test_guest_can_not_register_password_repeat_did_not_match(browser, email, password_valid, error_registration_text, error_registration_message_below_password_did_not_match):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasePage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
     password1 = password_valid
     password2 = f'{password_valid}111'
     login_page.new_user_registration(email, password1, password2)
     error_registration_message = login_page.should_not_be_registration_successful()
     assert error_registration_text == error_registration_message, 'Wrong error registration message'
     assert error_registration_message_below_password_did_not_match == login_page.should_be_error_registration_message_below(), 'Wrong error registration message below'
+
+@pytest.mark.need_review_custom_scenarios
+def test_guest_can_not_register_password_is_too_short(browser, email, password_short, error_registration_text, error_registration_message_below_password_is_too_short):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasePage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    password1 = password2 = password_short
+    login_page.new_user_registration(email, password1, password2)
+    error_registration_message = login_page.should_not_be_registration_successful()
+    assert error_registration_text == error_registration_message, 'Wrong error registration message'
+    assert error_registration_message_below_password_is_too_short == login_page.should_be_error_registration_message_below(), 'Wrong error registration message below'
+
+@pytest.mark.need_review_custom_scenarios
+def test_guest_can_not_register_password_is_too_common(browser, email, password_common, error_registration_text, error_registration_message_below_password_is_too_common):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasePage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    password1 = password2 = password_common
+    login_page.new_user_registration(email, password1, password2)
+    error_registration_message = login_page.should_not_be_registration_successful()
+    assert error_registration_text == error_registration_message, 'Wrong error registration message'
+    assert error_registration_message_below_password_is_too_common == login_page.should_be_error_registration_message_below(), 'Wrong error registration message below'
 
 @pytest.mark.need_review_custom_scenarios
 def test_user_can_login(browser, email_already_exist, password_valid, welcome_massage_when_user_login):
@@ -57,3 +93,5 @@ def test_user_can_login(browser, email_already_exist, password_valid, welcome_ma
     assert login_page.check_should_be_logout_link(), "Logout link is not presented"
     assert login_page.check_should_not_be_login_link(), "Login link is presented, but should not be"
     assert welcome_massage_when_user_login == login_page.should_be_login_successful(), 'Wrong welcome message'
+
+
