044927dc4fa2deb218ee2d1bd38a5810c720f724
teplota <1729@inbox.ru>
2020-07-22 10:49:56 +0300

    Add methods for basket_page, autorized_user

--- week_5/pages/base_page.py
+++ week_5/pages/base_page.py
@@ -1,5 +1,11 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from .locators import BasePageLocators
+
+
 import math
 
 class BasePage():
@@ -17,7 +23,15 @@ class BasePage():
             self.browser.find_element(how, what)
         except NoSuchElementException:
             return False
+
         return True
+
+    def is_not_element_present(self, how, what, timeout=10):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
 
     def check_current_url(self):
         url = self.browser.current_url
@@ -40,3 +54,20 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def go_to_basket_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.BASKET_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                 " probably unauthorised user"
+
