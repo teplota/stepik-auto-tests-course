d583b1abc6bf7555fa30c8f4ca0105423a8be538
teplota <1729@inbox.ru>
2020-07-22 10:52:22 +0300

    Add methods for check autorization, check error messages

--- week_5/pages/login_page.py
+++ week_5/pages/login_page.py
@@ -1,9 +1,18 @@
 from .base_page import BasePage
-from .locators import MainPageLocators
+from .locators import BasePageLocators
 from .locators import LoginPageLocators
 from selenium.webdriver.common.by import By
 
 class LoginPage(BasePage):
+
+    def enter_email(self, email):  # Ввести емейл
+        assert self.is_element_present(*LoginPageLocators.EMAIL_LOGIN), "Email field is not presented"
+        self.browser.find_element(*LoginPageLocators.EMAIL_LOGIN).send_keys(email)
+
+    def enter_password(self, password):  # Ввести пароль
+        assert self.is_element_present(*LoginPageLocators.PASSWORD_LOGIN), "Password field is not presented"
+        self.browser.find_element(*LoginPageLocators.PASSWORD_LOGIN).send_keys(password)
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
@@ -22,20 +31,8 @@ class LoginPage(BasePage):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
 
-    def test_user_should_login_successfully(self, email, password):
-        self.enter_email(email)                                # Ввести емейл
-        self.enter_password(password)                          # Ввести пароль
-        self.submit_registration_button()                      # Нажать на кнопку "Войти"
 
-    def enter_email(self, email):                              # Ввести емейл
-        assert self.is_element_present(*LoginPageLocators.EMAIL_LOGIN), "Email field is not presented"
-        self.browser.find_element(*LoginPageLocators.EMAIL_LOGIN).send_keys(email)
-
-    def enter_password(self, password):                          # Ввести пароль
-        assert self.is_element_present(*LoginPageLocators.PASSWORD_LOGIN), "Password field is not presented"
-        self.browser.find_element(*LoginPageLocators.PASSWORD_LOGIN).send_keys(password)
-
-    def submit_login_button(self):                                # Нажать на кнопку "Войти"
+    def submit_login_button(self):   # Нажать на кнопку "Войти"
         assert self.is_element_present(*LoginPageLocators.LOGIN_BUTTON), "Login button is not presented"
         self.browser.find_element(*LoginPageLocators.LOGIN_BUTTON).click()
 
@@ -44,6 +41,11 @@ class LoginPage(BasePage):
         self.enter_registration_password1(password1)  # Ввести пароль
         self.enter_registration_password2(password2)  # Повторить пароль
         self.submit_registration_button()  # Нажать на кнопку "Зарегистрироваться"
+
+    def existed_user_login(self, email, password):
+        self.enter_email(email)                                # Ввести емейл
+        self.enter_password(password)                          # Ввести пароль
+        self.submit_login_button()                             # Нажать на кнопку "Войти"
 
     def enter_registration_email(self, email):                              # Ввести емейл
         assert self.is_element_present(*LoginPageLocators.EMAIL), "Email field is not presented"
@@ -75,3 +77,20 @@ class LoginPage(BasePage):
         assert self.is_element_present(*LoginPageLocators.ERROR_REGISTRATION_MESSAGE_BELOW), "Error registration message below is not presented"
         error_registration_message_below = self.element_text(*LoginPageLocators.ERROR_REGISTRATION_MESSAGE_BELOW)
         return error_registration_message_below
+
+    def check_should_be_logout_link(self):
+        return self.is_element_present(*BasePageLocators.LOGOUT_LINK)
+
+    def check_should_not_be_login_link(self):
+        return self.is_not_element_present(*BasePageLocators.LOGIN_LINK)
+
+    def should_be_login_successful(self):
+        assert self.is_element_present(*BasePageLocators.WELCOME_MESSAGE), "Welcome message is not presented"
+        welcome_message = self.element_text(*BasePageLocators.WELCOME_MESSAGE)
+        return welcome_message
+
+
+
+
+
+
