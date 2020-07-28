5bdd7acb2b998b1f485ff57df64e171d3d74af35
teplota <1729@inbox.ru>
2020-07-28 23:17:51 +0300

    Added locators for checkout page

--- week_5/pages/locators.py
+++ week_5/pages/locators.py
@@ -7,11 +7,32 @@ class BasePageLocators():
     LOGOUT_LINK = (By.CSS_SELECTOR, "#logout_link")
     WELCOME_MESSAGE = (By.CSS_SELECTOR, "#messages .alert-success")
     USER_ICON = (By.CSS_SELECTOR, ".icon-user")
-    BASKET_LINK = (By.LINK_TEXT, "View basket")
+    #BASKET_LINK = (By.LINK_TEXT, "View basket")
+    BASKET_LINK = (By.XPATH, "//span/a[contains(@href,'basket')]")
+    #//span/a[contains(@href,'basket')]
+    #.basket - mini.btn - default
+    CHECKOUT_LINK = (By.XPATH, "//p/a[contains(@href,'checkout')]")
 
 class BasketPageLocators():
     BASKET_ITEMS = (By.CSS_SELECTOR, ".basket-items")
     EMPTY_BASKET_MESSAGE = (By.CSS_SELECTOR, ".content #content_inner")
+
+
+class CheckoutPageLocators():
+    FIRST_NAME = (By.ID, 'id_first_name')
+    LAST_NAME = (By.ID, 'id_last_name')
+    FIRST_LINE_ADDRESS = (By.ID, 'id_line1')
+    CITY = (By.ID, 'id_line4')
+    POSTCODE = (By.ID, 'id_postcode')
+    COUNTRY = (By.ID, 'id_country')
+    CONTINUE_BUTTON = (By.CSS_SELECTOR, ".form-group .btn")
+    PAYMENT_CONTINUE_BUTTON = (By.CSS_SELECTOR, ".btn")
+    ADDRESS_FORM = (By.XPATH, "//address")
+    ORDER_CONTENTS = (By.CSS_SELECTOR, ".basket-items h3 a")
+    ORDER_TOTAL = (By.CSS_SELECTOR, ".total .price_color")
+    ORDER_CONFIRMATION = (By.CSS_SELECTOR, ".lead")
+    PLACE_ORDER_BUTTON = (By.CSS_SELECTOR, "#place-order")
+
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
@@ -33,4 +54,3 @@ class ProductPageLocators():
     ADD_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form button")
     MESSAGE_TITLE = (By.CSS_SELECTOR, "#messages .alert-success strong")
     MESSAGE_PRICE = (By.CSS_SELECTOR, "#messages .alert-info strong")
-
