544481e9c3b4443b7fcb7da6ab7e336bbbaff33b
teplota <1729@inbox.ru>
2020-07-30 00:33:28 +0300

    Change assert error message

--- week_5/pages/basket_page.py
+++ week_5/pages/basket_page.py
@@ -8,8 +8,6 @@ class BasketPage(BasePage):
         return self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS)
 
     def should_be_message_empty_basket(self):
-        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET_MESSAGE), "Empty basket message is not presented"
+        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET_MESSAGE), "'Empty basket' message is not presented"
         empty_basket_message = self.element_text(*BasketPageLocators.EMPTY_BASKET_MESSAGE)
         return empty_basket_message
-
-
